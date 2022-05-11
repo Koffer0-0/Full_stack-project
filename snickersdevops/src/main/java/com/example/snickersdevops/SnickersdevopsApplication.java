62f242c3cede64e81a387b39dea5138c8623d243
Koffer0-0 <68071028+Koffer0-0@users.noreply.github.com>
2022-05-11 22:24:42 +0600

    кароч создал проект

---
+++ snickersdevops/src/main/java/com/example/snickersdevops/SnickersdevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.snickersdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnickersdevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SnickersdevopsApplication.class, args);
+    }
+
+}
---
+++ snickersdevops/src/test/java/com/example/snickersdevops/SnickersdevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.snickersdevops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnickersdevopsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
